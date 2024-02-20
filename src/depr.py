1264cff977ab67d9ab5e89cc0d35f17252e019b2
Hamish Gibbs <hamishgibbs@gmail.com>
2024-02-20 10:08:27 +0000

    docs on optimization

--- src/depr.py
+++ src/depr.py
@@ -10,19 +10,23 @@ def calc_waiting_time(beta, tau):
 def preferential_exploration(current_location, pij_weights):
     """
     Preferential exploration based on gravity model
+
+    Note: 
+    Choosing a destination is based on weights (horizontal). 
+    But here, ordering of dictionary keys (vertical) is used for destination names
+    This speeds up destination lookup, but requires that the dictionary keys 
+    have the same ordering as the column names in the pij dataframe
     """
     weights = np.array(pij_weights[current_location])
-
-    # Choosing destination (column) based on weights. 
-    # Requires that pij_weights.keys() values correspond to the ordering of weights
     return np.random.choice(list(pij_weights.keys()), size=1, p=weights)[0]
 
 def preferential_return(trips):
     """
     Preferential return based on frequency of previously visited locations
+
+    Weight is based on the number of times a location has been visited
     """
     
-    # weight is based on the number of times a location has been visited
     weights = np.unique(trips[:, 1], return_counts=True)[1]
     weights = weights / np.sum(weights)
 
@@ -66,13 +70,15 @@ def depr(uid, start_location, pij_weights, rho, gamma, beta, tau, duration, all_
 def population_depr(pop_sample, pij_weights, rho, gamma, beta, tau, duration):
     """
     Simulate a population of individuals based on the DEPR model
+
+    Note: 
+    Pre-allocates memory based on the assumption that each individual will make 10 trips 
+    This is an optimization dependent on duration and waiting time distribution
+    Too many trips per-individual will raise a ValueError
     """
 
     n_uids = pop_sample['pop_sample'].sum()
     
-    # NOTE: Pre-allocating memory to speed up simulation. 
-    # Estimating that each individual will make 10 trips 
-    # This is an optimization dependent on duration and waiting time distribution
     max_trips = n_uids * 10 
 
     all_trips = np.zeros(max_trips, dtype=[('uid', 'i4'), ('time', 'f4'), ('geoid', 'U10')])
@@ -98,6 +104,10 @@ def population_depr(pop_sample, pij_weights, rho, gamma, beta, tau, duration):
 
 
 def sample_population(pop, pop_sample_rate):
+    """
+    Sample origin locations of simulated individuals 
+    based on population density and a sampling rate
+    """
     
     pop = pop.with_columns(
         (pop['POPESTIMATE2019'] * pop_sample_rate).cast(pl.Int32).alias('pop_sample')
@@ -159,11 +169,10 @@ if __name__ == '__main__':
     pop = pop.filter(pop['GEOID'].str.slice(0, 2).is_in(states))
 
     pop_sample = sample_population(pop, POP_SAMPLE_RATE)
+
     print(f"Simulating {pop_sample['pop_sample'].sum():,} individuals")
 
     pij_weights = build_pij_weights(pij)
-    
-    # TODO: queue this up for parallel processing with cores provided by job
 
     all_trips = population_depr(
         pop_sample, 
